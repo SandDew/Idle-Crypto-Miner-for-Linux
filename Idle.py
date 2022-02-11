990aedf5ba6678cdba22e7c6462c863ba71c8e4e
Andrew <68248474+Chocolatebar9001@users.noreply.github.com>
2022-02-11 00:57:33 +0000

    Update Idle.py

--- Idle.py
+++ Idle.py
@@ -1,3 +1,5 @@
+#OH BOY HOPE YOU LIKE SPAGHETTI
+
 import subprocess
 import time
 import psutil
@@ -17,17 +19,17 @@ y=0 #while true didnt work
 while y == 0:
     x=subprocess.check_output(['xprintidle'])
     z=float(x) #if statements require float to work
-    if z >= 3000:
+    if z >= 300000: #You're going to wait five minutes and you are going to like it! (unless you change the value-my only weakness)
         if checkIfProcessRunning('xmr-stak-rx'):
             time.sleep(1)
         else :
-            subprocess.run(['./xmr-stak-rx']) #Fix to run separately
-            print('Started program')
+            subprocess.Popen(['./xmr-stak-rx --noTest'])
+            print("\n" + 'Started miner' + "\n")
             time.sleep(1)
     else :
         if checkIfProcessRunning('xmr-stak-rx'):
-            subprocess.run(['killall xmr-stak-rx'])
-            print('Killed program')
+            subprocess.run(['killall xmr-stak-rx'], shell=True) #I just wasted two hours of my time looking for workarounds for this line, just to find out I only needed to add ", shell=True". (╯°□°)╯︵ ┻━┻
+            print("\n" + 'Killed miner' + "\n")
             time.sleep(1)
         else:
             time.sleep(1)
